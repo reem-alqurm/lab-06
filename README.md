93b3c15d27b878415e4a0532974a0b0a448877be
Your-Full-Name <r.alqerm@gmail.com>
2021-03-29 02:13:04 +0300

    all fetures solved

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
 # lab-06
+
+## Number and name of feature: feature # 1
+
+## Estimate of time needed to complete: 1 houre
+
+Start time: 9:00PM
+
+Finish time: 10:00 PM
+
+Actual time needed to complete: 1 hour
+
+*************************************************** 
+Number and name of feature: feature # 2
+
+Estimate of time needed to complete: 1 houre
+
+Start time: 10:00PM
+
+Finish time: 12:00 PM
+
+Actual time needed to complete: 2 hour
+***************************************************
+Number and name of feature: feature # 3
+
+Estimate of time needed to complete: 1 houre
+
+Start time: 12:00AM
+
+Finish time: 1:00 AM
+
+Actual time needed to complete: 1 hour
+***************************************************
+umber and name of feature: feature # 4
+
+Estimate of time needed to complete: 1 houre
+
+Start time: 1:00 AM
+
+Finish time: 2:00 AM
+
+Actual time needed to complete: 1 hour
+***************************************************
+# Project Name
+
+**Author**: Reem Alqurm
+**Version**: 1.0.0 
+
+## Overview
+
+* get the inputed data from a user and 
+
+## Getting Started
+<!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
+
+## Architecture
+<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
+
+## Change Log
+<!-- Use this area to document the iterative changes made to your application as each feature is successfully implemented. Use time stamps. Here's an examples:
+
+01-01-2001 4:59pm - Application now has a fully-functional express server, with a GET route for the location resource.
+
+## Credits and Collaborations
+<!-- Give credit (and a link) to other people or resources that helped you build this application. -->
+-->
---
+++ server.js
@@ -0,0 +1,52 @@
+'use strict'
+const express = require('express') // git things from express
+const cors = require('cors'); // Api call our of domian
+
+const app = express(); // to use the express 
+app.use(cors());//to make the app to talke out of domin 
+const port = 7000; // to resive a port for my server (pc)
+app.listen( port , ()=>{console.log('conected server .......')});//to conect to server
+
+function CityExpoler(search_query, formatted_query, latitude, longitude) {
+    this.search_query = search_query,
+    this.formatted_query = formatted_query,
+    this.latitude = latitude,
+    this.longitude = longitude
+}
+
+app.get('/location',getLocation);//to get the data (API)
+
+function getLocation(requst , response ){
+const loc = require('./data/location.json');
+const search_query = requst.query.city; //get the name that user entered in input feild 
+    const formatted_query = loc[0].display_name;
+    const latitude = loc[0].lat;
+    const longitude = loc[0].lon;
+    let cityLocation = new CityExpoler(search_query, formatted_query, latitude, longitude);
+    response.send(cityLocation); //send the data to the frontend side to display for the user 
+}
+
+function Weathercity(time, description) {
+    this.time = time,
+    this.description = description
+}
+app.get('/weather' , getWeather);
+
+function getWeather (requst , response){
+    const weth = require('./data/weather.json');
+    let array = [];
+
+    let data = weth.data;
+    data.forEach(((item) => {
+        // eslint-disable-next-line no-unused-vars
+        let weather = new Weathercity(item.datetime,item.weather.description);
+        array.push({ 'time': item.datetime, 'forecast': item.weather.description })
+    }))
+    response.send(array);
+}
+
+//error handler
+app.use('*', (requst, response) => {
+    let status = 404;
+    response.status(status).send({status:status , msg:'Not found'});
+  });
